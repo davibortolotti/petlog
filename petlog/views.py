0145309ec9845618162ac3e5295d47928b2ef6f8
Davi Bortolotti <davi.bortolotti@simplelegal.com>
2023-11-27 07:20:27 -0300

    Fixes card layout on home screen, adds vaccine cards to home screen

--- petlog/views.py
+++ petlog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.db.models import Value
 
 from petlog.logger import models
 
 
 def home(request):
-    entries = models.Entry.objects.all().order_by('-created_on')
-    context = {"entries": entries}
+    
+    entries = models.Entry.objects.all().order_by('-created_on').annotate(type=Value('entry'))
+    vaccines = models.Vaccine.objects.all().order_by('-created_on').annotate(type=Value('vaccine'))
+    events = list(entries) + list(vaccines)
+    events.sort(key=lambda x: x.created_on, reverse=True)
+    context = {"events": events}
     return render(request, "home/home.html", context)
