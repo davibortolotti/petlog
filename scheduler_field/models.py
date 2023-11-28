f152278cfc5ea6367df5b9c52949837d244c3b44
Davi Bortolotti <davi.bortolotti@simplelegal.com>
2023-11-28 14:04:00 -0300

    Split scheduler field into a different repository

--- scheduler_field/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class SchedulerDateManager(models.Model):
-    table_name = models.CharField(max_length=100)
-    method_name = models.CharField(max_length=50)
-    field_name = models.CharField(max_length=50)
