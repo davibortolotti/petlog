d7f0080ceb0426b2236e25720f87c6294ac503b3
Davi Bortolotti <davi.bortolotti@simplelegal.com>
2023-12-01 14:46:35 -0300

    Adds deletion templates/views to every type of entry

--- petlog/logger/urls.py
+++ petlog/logger/urls.py
@@ -6,5 +6,8 @@ from petlog.logger import views
 urlpatterns = [
     path("create_entry/", views.EntryCreateView.as_view(), name='create_entry'),
     path("create_vaccine/", views.VaccineCreateView.as_view(), name='create_vaccine'),
-    path("create_training/", views.TrainingCreateView.as_view(), name='create_training')
+    path("create_training/", views.TrainingCreateView.as_view(), name='create_training'),
+    path("delete_entry/<int:pk>/", views.EntryDeleteView.as_view(), name='delete_entry'),
+    path("delete_vaccine/<int:pk>/", views.VaccineDeleteView.as_view(), name='delete_vaccine'),
+    path("delete_training/<int:pk>/", views.TrainingDeleteView.as_view(), name='delete_training'),
 ]
--- petlog/logger/views.py
+++ petlog/logger/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, DeleteView
 
 from petlog.logger.forms import EntryForm, VaccineForm, TrainingForm
+from petlog.logger.models import Vaccine, Entry, Training
+
 
 class HomeViewMixin:
     def form_valid(self, form):
@@ -16,11 +18,12 @@ class HomeViewMixin:
 
     def get_success_url(self):
         return reverse_lazy('home')
-    
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs.update({'user': self.request.user})
         return kwargs
+
 
 class EntryCreateView(HomeViewMixin, LoginRequiredMixin, FormView):
     form_class = EntryForm
@@ -36,3 +39,20 @@ class VaccineCreateView(HomeViewMixin, LoginRequiredMixin, FormView):
     form_class = VaccineForm
     template_name = 'logger/create_vaccine.html'
 
+
+class VaccineDeleteView(DeleteView):
+    model = Vaccine
+    template_name = 'logger/delete_vaccine.html'
+    success_url = reverse_lazy("home")
+
+
+class EntryDeleteView(DeleteView):
+    model = Entry
+    template_name = 'logger/delete_entry.html'
+    success_url = reverse_lazy("home")
+
+
+class TrainingDeleteView(DeleteView):
+    model = Training
+    template_name = 'logger/delete_training.html'
+    success_url = reverse_lazy("home")
